dc90cc5b04356c76d6ca75dd87dc4bc42f9d405b
Pantea Khodabakhsh <pantea.khodabakhsh@student.ap.be>
2026-03-10 10:30:56 +0100

    login

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,23 +1,39 @@
 "use strict";
 
-let projectsMusicMatch = document.querySelector(".projects.musicMatch");
+const projectsMusicMatch = document.querySelector(".projects.musicMatch");
 const modal = document.getElementById("loginModal");
 const modalContent = document.querySelector(".modal-content");
-const close = document.querySelector(".close");
+const close1 = document.querySelector(".close-1");
+const close2 = document.querySelector(".close-2");
 const loginButton = document.querySelector("#login");
+const projectA = document.querySelector(".projects.project-a");
+const projectB = document.querySelector(".projects.project-b");
+const accessDenied = document.querySelector(".access-denied");
 
 projectsMusicMatch.addEventListener("click", () => {
   modal.style.display = "flex";
-  const close = document.querySelector(".close");
-  close.addEventListener("click", () => {
+  close1.addEventListener("click", () => {
     modal.style.display = "none";
   });
 });
 
 loginButton.addEventListener("click", () => {
   modal.style.display = "flex";
-  const close = document.querySelector(".close");
-  close.addEventListener("click", () => {
+  close1.addEventListener("click", () => {
     modal.style.display = "none";
+  });
+});
+
+projectA.addEventListener("click", () => {
+  accessDenied.style.display = "flex";
+  close2.addEventListener("click", () => {
+    accessDenied.style.display = "none";
+  });
+});
+
+projectB.addEventListener("click", () => {
+  accessDenied.style.display = "flex";
+  close2.addEventListener("click", () => {
+    accessDenied.style.display = "none";
   });
 });
