36c3dd95d1466a30059916d2e413538e5b462d56
Pantea Khodabakhsh <pantea.khodabakhsh@student.ap.be>
2026-03-10 12:24:48 +0100

    login update

--- scripts/general.js
+++ scripts/general.js
@@ -2,17 +2,41 @@
 
 const loginButton = document.querySelector("#login");
 const modal = document.getElementById("loginModal");
-const modalContent = document.querySelector(".modal-content");
 const close1 = document.querySelector(".close-1");
 const logo = document.querySelector(".logo");
+const loginModalButton = document.querySelector(".login-button");
+const errormessage = document.querySelector("#error-success");
+
+const usernameTest = "pantea@ap.be";
+const passwordTest = "1234";
 
 logo.addEventListener("click", () => {
   window.location.href = "./searchPage.html";
 });
 
 loginButton.addEventListener("click", () => {
+  errormessage.textContent = "";
+  document.querySelector("#password").value = "";
+  document.querySelector("#username").value = "";
+  errormessage.className = "";
   modal.style.display = "flex";
-  close1.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
+});
+
+loginModalButton.addEventListener("click", () => {
+  const userName = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  errormessage.className = "";
+
+  if (userName !== usernameTest || password !== passwordTest) {
+    errormessage.classList.add("red");
+    errormessage.textContent = "Uw gegevens klopt niet!";
+  } else {
+    errormessage.classList.add("green");
+    errormessage.textContent = "Je hebt successful ingelogd!";
+  }
+});
+
+close1.addEventListener("click", () => {
+  modal.style.display = "none";
 });
