afbf893c565fdd4968035a078c8686dca51b19fc
Samar Asla <samar.asla@student.ap.be>
2026-03-13 22:00:55 +0100

    search page aanpassen

--- scripts/SpotifyService.js
+++ scripts/SpotifyService.js
@@ -1,5 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQChNXwCy371cBF4sS0dxDeRbjzkTrytUvD_F_du7YZJBks2LRiFMSNNnjjRtvsKomO0M-99F48P65y_W_BKWQfJIzcjIHnCs30p0YaS7Fx48v4Z2EnNXQHxluPGun3uKmBz0YBRYNoZO8ahDxWhXEoE5fOHr1FT0o1tSJRDEYrzyXAIbR8kFIXvoZN78Q0KYRNOjq8U_oa4oAJxLtX5MWnBBe20uPFbbN1y3CABeHMTaNTxLhaeCtMufXudh4W05JDhFVS4LEmcGR21BMAEDL8eNzW1iJjok91jBVxAug_D5RGj3wRC-ENUEVtsfRUWvEqX';
+const token = 'BQAEAXYt4sgDELOkRgkmoqg5wuT17i7VrCgDWqhaH71K1VkzIXwUgLmrwQOC52Uw-KpPfpxO-IHtS-cIvChp-iDYTyuQbb3Y6TTEWlLY8nB0XmXGiWbnL8FVy62OdcP7Fno4A1fZ-fKgqmTQj-hd_xtxKMZop3d2GCNQ01EwF-fOIk7iW1Kr7K11NQ1RGSt2zEUdRiHgiNwwYvQPbTRNGcf9LA33cn6LAWjceLCFzWza2V4x9UHCB2B0uXnCzRw71qH-nRYcBRPKRzf3WO77k4Tl1G6o-EI9Jn-U5wW0EOnoygCZmzbWsSF-VU8hNjfoEFPz'
 const headers = {
   Authorization: `Bearer ${token}`,
 };
