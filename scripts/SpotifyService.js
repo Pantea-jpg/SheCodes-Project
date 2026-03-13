96e606a9e32ba350d1650babba80625020e07ccd
Samar Asla <samar.asla@student.ap.be>
2026-03-13 21:24:30 +0100

    search en generator page aanpassen

--- scripts/SpotifyService.js
+++ scripts/SpotifyService.js
@@ -1,7 +1,5 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token =
-  "BQCdUnIEVJ4f2grd2IuQTurynyRZPkCzHrFq01ZuyQahvkBZqkjtGno8VSkRJH70HYRyQ7vggH-hvsRDCujXDHosvO72MCJG_rPNOjeHInF81vZo7N9Z_rA_q5-Y5wiySSLQtgf3dkngfeVxH5Coq3uskfb1NfdfGqhEbM9zNDQJpzGEjWg89ZwWeM2tYCP91PFNAVxnFIps-h9L1uQvm1QyMmiU94i3DnEEy1ZMIGnddfv8shk8BHzFTStOGL9F1Hoz8xCyeRxI5dMeK-u75n9UJqkrIzXIShINRH9dxUkl3YUA5NzCc7kLq7R5JvxnNAwQ";
-
+const token = 'BQChNXwCy371cBF4sS0dxDeRbjzkTrytUvD_F_du7YZJBks2LRiFMSNNnjjRtvsKomO0M-99F48P65y_W_BKWQfJIzcjIHnCs30p0YaS7Fx48v4Z2EnNXQHxluPGun3uKmBz0YBRYNoZO8ahDxWhXEoE5fOHr1FT0o1tSJRDEYrzyXAIbR8kFIXvoZN78Q0KYRNOjq8U_oa4oAJxLtX5MWnBBe20uPFbbN1y3CABeHMTaNTxLhaeCtMufXudh4W05JDhFVS4LEmcGR21BMAEDL8eNzW1iJjok91jBVxAug_D5RGj3wRC-ENUEVtsfRUWvEqX';
 const headers = {
   Authorization: `Bearer ${token}`,
 };
@@ -14,15 +12,3 @@ export async function searchSpotify(query) {
   const res = await fetch(url, { headers });
   return res.json();
 }
-
-//generator page
-// export async function getRecommendations({ genre, tempo, popularity }) {
-//   const url = `https://api.spotify.com/v1/recommendations?limit=10&seed_genres=${genre}&target_tempo=${tempo}&target_popularity=${popularity}`;
-//   const res = await fetch(url, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   return res.json();
-// }
