0e021fc0ccacfcf16f464160f0cf035ed7033aed
Samar Asla <samar.asla@student.ap.be>
2026-03-15 13:05:25 +0100

    generator page aanpassen

--- scripts/SpotifyService.js
+++ scripts/SpotifyService.js
@@ -1,14 +1,26 @@
 // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQAEAXYt4sgDELOkRgkmoqg5wuT17i7VrCgDWqhaH71K1VkzIXwUgLmrwQOC52Uw-KpPfpxO-IHtS-cIvChp-iDYTyuQbb3Y6TTEWlLY8nB0XmXGiWbnL8FVy62OdcP7Fno4A1fZ-fKgqmTQj-hd_xtxKMZop3d2GCNQ01EwF-fOIk7iW1Kr7K11NQ1RGSt2zEUdRiHgiNwwYvQPbTRNGcf9LA33cn6LAWjceLCFzWza2V4x9UHCB2B0uXnCzRw71qH-nRYcBRPKRzf3WO77k4Tl1G6o-EI9Jn-U5wW0EOnoygCZmzbWsSF-VU8hNjfoEFPz'
+const token = 'BQDcsCndJv7oX5Z6XRsh114HmgLnzIzRlUqYyQysI4vh0drCsJRkJW7APu0Ay5d2uV2QhKqFt2UUy2mxtBv8aoYcU4gXH0dHJiZGktgHBi4UknnEhKbNyacroyVQm0I7zz2K8CH_1JC0PeXJNgeFEOCwmtZZB43LM_cGIyOWV-h-SFUxokw6DDOuBbT_DgjHPK4QTH4wIsBi-YDSdRPP6Psf5vmLIw9NiT4AK34jfYOS1haftwBz1JOuYB7lqDjr5pDeYdk9KBl8KvR8aPbIxKadn_Jpwxl_cZw9H23X7wZbTXrIea_Un7q1BeHkqBzpBjB8';
 const headers = {
   Authorization: `Bearer ${token}`,
 };
+
 // voor search page
 export async function searchSpotify(query) {
   const url = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
     query,
   )}&type=track,artist&limit=10`;
 
+  const res = await fetch(url, { headers });
+  return res.json();
+}
+
+export async function getTrendingTracks() {
+  const url = `https://api.spotify.com/v1/search?q=genre:pop&type=track&limit=10`;
+  const res = await fetch(url, { headers });
+  return res.json();
+}
+export async function getPopularArtists() {
+  const url = `https://api.spotify.com/v1/search?q=pop&type=artist&limit=10`;
   const res = await fetch(url, { headers });
   return res.json();
 }
--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,10 @@
-import { searchSpotify } from "./SpotifyService.js";
+// import { searchSpotify } from "./SpotifyService.js";
+import { 
+  searchSpotify, 
+  getTrendingTracks, 
+  getPopularArtists 
+} from "./SpotifyService.js";
+
 
 
 
@@ -39,7 +45,10 @@ artistsButton.addEventListener("click", () => {
 
 // Renderfunctie
 function renderResults() {
-  if (!lastSearchData) return;
+ if (!lastSearchData) {
+  renderHomeSections();
+  return;
+}
 
   resultsContainer.innerHTML = ""; // leegmaken
 
@@ -79,3 +88,59 @@ function renderResults() {
     });
   }
 }
+
+
+
+//  HOME DATA
+// =======================
+async function loadHomeData() {
+  const trendingData = await getTrendingTracks();
+  const artistsData = await getPopularArtists();
+
+  return {
+    trendingTracks: trendingData.tracks.items.slice(0, 3),
+    popularArtists: artistsData.artists.items.slice(0, 3)
+  };
+}
+//  HOME SECTIONS
+// =======================
+async function renderHomeSections() {
+  const homeData = await loadHomeData();
+
+  const trending = homeData.trendingTracks;
+  const artists = homeData.popularArtists;
+
+  resultsContainer.innerHTML = `
+    <button class="create-playlist-btn">🪄 Maak een playlist</button>
+
+    <h2 class="section-title">🔥 Trending nummers</h2>
+    <div class="home-row">
+      ${trending.map(t => `
+        <div class="home-card">
+          <img src="${t.album.images[1]?.url || t.album.images[0]?.url}" class="home-img">
+          <p>${t.name} - ${t.artists.map(a => a.name).join(", ")}</p>
+        </div>
+      `).join("")}
+    </div>
+
+  <h2 class="section-title">🌍 Populaire artiesten</h2>
+    <div class="home-row">
+      ${artists.map(a => `
+        <div class="home-card">
+          <img src="${a.images?.[0]?.url || "./assets/default.png"}" class="home-img round">
+          <p>${a.name}</p>
+        </div>
+      `).join("")}
+    </div>
+
+   
+  `;
+
+const createBtn = document.querySelector(".create-playlist-btn");
+
+createBtn.addEventListener("click", () => {
+  window.location.href = "./generator.html"; 
+});
+}
+
+renderHomeSections();
